5371f11e9e0c28d74191ceb66820872b2d6418dc
Vitaliy14062005 <vitaliy.ivaskiv@edu.uah.es>
2024-10-11 09:55:07 +0200

    Update Lista.hpp

--- PL1/Lista.hpp
+++ PL1/Lista.hpp
@@ -1,12 +1,23 @@
-#ifndef LISTA_HPP
 #define LISTA_HPP
 
-class Lista
-{
-public:
-	Lista();
-	~Lista();
+#include "NodoLista.hpp"
+using namespace std;
 
+class Lista {
+public:
+    Lista();  
+    ~Lista(); 
+
+    void agregar(int valor);  
+    int extraer();           
+    int primero();           
+    void mostrar() const;     
+    int getLongitud() const;  
+    bool vacia() const;       
+
+private:
+    pnodoLista cabeza;  
+    int longitud;      
 };
 
 #endif // LISTA_HPP
